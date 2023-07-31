ede0dc851bd5b0506e5ec3047e8c2edba1a85897
Matheus Palermo <matheus.palermo@dbccompany.com.br>
2023-07-31 11:18:35 -0300

    dependencie gh-pages removed

--- src/routes/consts.js
+++ src/routes/consts.js
@@ -1,6 +1,6 @@
-export const HOME = '/alura-challenge-front-end-5edicao'
-export const LOGIN = `${HOME}/login`
-export const ALL_PRODUCTS = `${HOME}/all-products`
-export const ADD_PRODUCT = `${HOME}/add-product`
-export const PRODUCT_DETAILS = `${HOME}/product-details`
-export const ERROR = `${HOME}/*?`
+export const HOME = '/'
+export const LOGIN = `/login`
+export const ALL_PRODUCTS = `/all-products`
+export const ADD_PRODUCT = `/add-product`
+export const PRODUCT_DETAILS = `/product-details`
+export const ERROR = `/*`
